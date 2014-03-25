9923069fcd3dbb759d9c0b3d580798c1fcbacb4f
Alex Bechanko <chanko08@gmail.com>
2014-03-24 22:00:30 -0400

    n dimensional nonsense

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 import qualified Data.Map as M
-import qualified System.Random as RNG
 import Data.List
 import Data.List.Split
+import Control.Applicative
 
 type Row = Integer
 type Col = Integer
@@ -10,17 +10,33 @@ type Power = Integer
 
 data Board = Board { numCols :: Int
 				   , numRows :: Int
-				   , board :: (M.Map (Row, Col) Power)	
+				   , board :: M.Map (Row, Col) Power
 				   }
 
-data NBoard = NBoard { size :: [Int]
-					 , board :: (M.Map [Int] Power)
-					 }
+type DimInd = Integer
+type NBoard = M.Map [DimInd] Power
 
-data NBoard a = Dim [Dim a] | Bottom a deriving Show
+data Dimension = Dim DimInd Dimension | DimEnd DimInd deriving (Eq, Ord, Show)
+
+coords :: [Integer] -> [Dimension]
+coords sizes = foldr toDim (makeEnds . head $ dims) $ tail dims
+  where
+    dims = map (\s -> [1..s]) sizes
+    makeEnds = map DimEnd
+    toDim ss dims = Dim <$> ss <*> dims
+
+{-
+coords []       = error "wtf mate"
+coords (x:y:[]) = DimEnd x y 
+coords (x:ys)   = Dim x . coords $ ys
+
+-}
+
+
 
 data Direction = LEFT | RIGHT | UP | DOWN
 
+{-{{{
 randomlyPlaceTile :: Board -> Board
 randomlyPlaceTile = undefined
 
@@ -35,8 +51,8 @@ fromListOfRows l = Board nc nr b
 	where
 		nc = length . head $ l
 		nr = length l
-		b  = M.fromList . concat . map toCoord . zip [1..] . map (zip [1..]) $ l
-		toCoord (r, cs) = map (\(c, v) -> ((r, c), v)) $ cs
+		b  = M.fromList . concatMap toCoord . zip [1..] . map (zip [1..]) $ l
+		toCoord (r, cs) = map (\(c, v) -> ((r, c), v))  cs
 
 toListOfCols :: Board -> [[Power]]
 toListOfCols b = transpose . toListOfRows $ b
@@ -48,12 +64,12 @@ toString :: Board -> String
 toString b = intercalate "\n" $ [top] ++ guts ++ [top]
 	where
 		guts = map formatRow . toListOfRows $ b
-		top  = "+" ++ (replicate ((length . head $ guts) - 2) '-') ++ "+"
+		top  = "+" ++ replicate ((length . head $ guts) - 2) '-' ++ "+"
 
 formatRow :: [Power] -> String
 formatRow r = "| " ++ nums ++ " |"
 	where
-		nums = intercalate " " . map formatNumber $ r
+		nums = unwords . map formatNumber $ r
 
 formatNumber :: Power -> String
 formatNumber 0 = padToLength 2 " "
@@ -63,11 +79,15 @@ padToLength :: Int -> String -> String
 padToLength n s = 
 	if length s < n 
 		then
-			(replicate (n - length s) ' ') ++ s
+			replicate (n - length s) ' ' ++ s
 		else
 			s
 
+main :: IO ()
 main = do
 	let x = [[1,0,0], [1,2,0], [3,2,1]]
 	let	b = fromListOfRows x
 	putStrLn . toString $ b
+}}}-}
+
+main = print "hello"
